566f5a7488eacaac85c59dec05cd7c538872c8a9
Rohit <91272492+rohit092021@users.noreply.github.com>
2026-03-10 12:15:22 +0000

    conversion

--- 01_basics/conversion_operation.js
+++ 01_basics/conversion_operation.js
@@ -12,3 +12,20 @@ let num=10;
 let convertInString = String(num)
 console.log(convertInString)
 console.log(typeof(convertInString))
+
+
+                // String to Number conversion
+
+let str1 = 2, str2="2";
+let str3 = str1+str2;
+console.log(str3)
+console.log("1"+2+2)
+console.log(1+2+"3")
+console.log(+true)
+console.log(+"")
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num1)
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
