6a8632ad81030610a777ee23c78948cb2b338e1d
Rohit <91272492+rohit092021@users.noreply.github.com>
2026-03-10 11:19:46 +0000

    DataType Conversion

---
+++ 01_basics/conversion_operation.js
@@ -0,0 +1,14 @@
+let score=24;
+console.log(typeof(score))
+console.log(typeof score)
+
+let score1="23asd"
+console.log(typeof score1)
+let convertInNumber = Number(score1)
+console.log(typeof(convertInNumber))
+console.log(convertInNumber)
+
+let num=10;
+let convertInString = String(num)
+console.log(convertInString)
+console.log(typeof(convertInString))
