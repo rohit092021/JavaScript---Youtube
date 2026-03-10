0111fd83e542eff99d128d85e178049e849782c8
Rohit <91272492+rohit092021@users.noreply.github.com>
2026-03-10 10:03:28 +0000

    DataTypes in JS

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,8 @@
+"USE Strict"; //Treate all JS code as a newer version 
+//alert(3+3);//it not works same as brouser. because we are using Node.js
+// total 8 data Types and 1 object
+// Number,string, Boolean, null, Undefined, bigint, symbol, object
+console.log(typeof(null))
+console.log(typeof(undefined))
+console.log(typeof('ss'))
+console.log(typeof(123))
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,1 +1,9 @@
-console.log("variables")
+const accountID = 2334
+let accountEmail= "rohit@gmail.com"
+var accountPass = "plm"// dont use var because of issue in block and function scope
+console.log("accountID")
+
+// accountID = 334
+accountEmail= "rohittarkar@gmail.com"
+accountPass = "plmlp"
+console.table([accountID, accountEmail, accountPass]);
