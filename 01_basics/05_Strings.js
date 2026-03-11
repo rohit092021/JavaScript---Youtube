8ff90df02e450865c799a5c384bbe7f35fc24f0b
Rohit <91272492+rohit092021@users.noreply.github.com>
2026-03-11 08:18:18 +0000

    commit

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,13 @@
+let firstName = 'Rohit'
+let lastName = 'Tarkar'
+console.log(firstName + " " + lastName)// this is the oldest way
+
+// use backticks
+console.log(`My first name is ${firstName} and Last Name is ${lastName}`)
+
+const gameName = new String('Rohit')
+console.log(gameName)
+console.log(gameName[0])
+console.log(gameName.__proto__)
+console.log(gameName.substring)
+// console.log(gameName.substring)
---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+Number
+
+const score = 400
+const balance = new Number(100)
+console.log(balance)
+
+// Math
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(5.4))
+console.log(Math.ceil(4.9))
+console.log(Math.floor(4.9))
+console.log(Math.random())
+console.log(Math.random()*10 )
+console.log(Math.random()*10+1)// +1 isliye taki jaise hme 1-10 tk value chahiye but random 0.033 aagya to total value 0.3 hi hogi
+
+const min=10
+const max=30
+console.log(Math.random()* (max - min + 1) + min)
