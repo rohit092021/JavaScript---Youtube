6887029a59e2f9016cc56f97bbe9d395a4b02042
Rohit <91272492+rohit092021@users.noreply.github.com>
2026-03-11 10:06:13 +0000

    date and time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,21 @@
+// dates
+let myDate = new Date()
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toJSON())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
+console.log(myDate.getTimezoneOffset())
+console.log(myDate.getHours())
+console.log(myDate.getMonth())
+console.log(myDate.toUTCString())
+
+let myCreatedDate = new Date(2004,3, 31)
+console.log(myCreatedDate.toDateString());
+
+let myCreatedDate_1 = new Date(2004,3, 31,4,5)
+console.log(myCreatedDate.toLocaleString());
